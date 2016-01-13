8e759e0c57cbca89ec53afdd2f7e08195f499622
Francisco Caiceo <jfcaiceo55@gmail.com>
2016-01-13 10:38:40 -0300

    Add image modifiers

--- src/Image.jsx
+++ src/Image.jsx
@@ -2,6 +2,7 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ImageModifiers = require('./ImageModifiers');
 
 module.exports = React.createClass({
   displayName: 'Image',
@@ -13,36 +14,104 @@ module.exports = React.createClass({
       loader: true,
       sizeX: '100%',
       sizeY: 'auto',
-      positionX: 'center',
-      positionY: ''
+      positionX: 0,
+      positionY: 0,
+      rotate: 0,
+      width: 0,
+      height: 0
     };
   },
   componentWillMount: function() {
-    this.updateImageInitialSize(this.props);
+    this.resetImageInitialState(this.props);
   },
   componentWillReceiveProps: function(nextProps) {
-    this.updateImageInitialSize(nextProps);
+    this.resetImageInitialState(nextProps);
   },
-  updateImageInitialSize: function(props) {
+  componentDidMount: function() {
+    this.startPoints = null;
+    document.addEventListener('mousedown', this.handleMouseDown);
+    document.addEventListener('mousemove', this.handleMouseMove);
+    document.addEventListener('mouseup', this.handleMouseUp);
+    document.addEventListener('wheel', this.handleWheel);
+  },
+  componentWillUnmount: function() {
+    document.removeEventListener('mousedown', this.handleMouseDown);
+    document.removeEventListener('mousemove', this.handleMouseMove);
+    document.removeEventListener('mouseup', this.handleMouseUp);
+    document.removeEventListener('wheel', this.handleWheel);
+  },
+  resetImageInitialState: function(props) {
     let img = new Image();
     let _this = this;
     img.onload = function(){
-      console.log(this.width);
-      console.log(this.height);
-      if(this.height > this.width)
-        _this.setState({
-          loader: false,
-          sizeX: 'auto',
-          sizeY: '100%'
-        })
-      else
-        _this.setState({
-          loader: false,
-          sizeX: '100%',
-          sizeY: 'auto'
-        })
+      let [sizeX, sizeY] = _this.getDefaultSize(this.width, this.height);
+      _this.setState({
+        loader: false,
+        sizeX: sizeX,
+        sizeY: sizeY,
+        rotate: 0,
+        width: this.width,
+        height: this.height
+      })
     };
     img.src = props.src;
+  },
+  handleRotate: function(angle) {
+    this.setState({
+      rotate: (360 + this.state.rotate + angle) % 360
+    })
+  },
+  handleZoom: function(direction) {
+    let percent = direction > 0 ? 5 : -5;
+    let sizeX = parseInt(this.state.sizeX);
+    let sizeY = parseInt(this.state.sizeY);
+    if(!isNaN(sizeX)) {
+      sizeX = `${sizeX + percent}%`;
+      sizeY = 'auto';
+    }
+    else {
+      sizeY = `${sizeY + percent}%`;
+      sizeX = 'auto';
+    }
+    this.setState({
+      sizeX: sizeX,
+      sizeY: sizeY
+    })
+  },
+  handleWheel: function(ev) {
+    if(this.isInsideImage(ev))
+      this.handleZoom(ev.deltaY);
+  },
+  handleMouseMove: function(ev) {
+    if(!this.startPoints)
+      return;
+    let state = this.state;
+    let posX = ev.pageX - this.startPoints[0];
+    let posY = ev.pageY - this.startPoints[1];
+    this.startPoints = [ev.pageX, ev.pageY]
+    this.setState({
+      positionX: state.positionX + posX,
+      positionY: state.positionY + posY
+    });
+  },
+  handleMouseUp: function(ev) {
+    this.startPoints = null;
+  },
+  handleMouseDown: function(ev) {
+    if(this.isInsideImage(ev))
+      this.startPoints = [ev.pageX, ev.pageY];
+  },
+  isInsideImage: function(ev) {
+    let rect = ReactDOM.findDOMNode(this).getBoundingClientRect();
+    if(ev.pageY < rect.top || ev.pageY > rect.bottom || ev.pageX < rect.left || ev.pageX > rect.right)
+      return false;
+    return true;
+  },
+  getDefaultSize: function(w, h) {
+    if(h > w)
+      return ['auto', '100%']
+    else
+      return ['100%', 'auto']
   },
   render: function() {
     let [props, state] = [this.props, this.state];
@@ -56,18 +125,25 @@ module.exports = React.createClass({
         </div>
       )
     }
+    let transform = `rotate(${state.rotate}deg)`;
     let styles = {
       height: '100%',
       backgroundImage: background,
       backgroundRepeat: 'no-repeat',
       backgroundSize: `${state.sizeX} ${state.sizeY}`,
-      backgroundPosition: `${state.positionX} ${state.positionY}`
+      backgroundPosition: `calc(50% + ${state.positionX}px) calc(50% + ${state.positionY}px)`,
+      MsTransform: transform,
+      WebkitTransform: transform,
+      transform: transform
     }
     return (
       <div className='lightbox-content-center'>
         <div className='lightbox-image-container' style={styles}>
           {loader}
         </div>
+        <ImageModifiers
+          handleRotate={this.handleRotate}
+          handleZoom={this.handleZoom}/>
       </div>
     )
   }
---
+++ src/ImageModifiers.jsx
@@ -0,0 +1,42 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+module.exports = React.createClass({
+  displayName: 'Image',
+  propTypes: {
+    handleRotate: React.PropTypes.func.isRequired,
+    handleZoom: React.PropTypes.func.isRequired
+  },
+  handleRotateClockwise: function() {
+    this.props.handleRotate.call(this, 90);
+  },
+  handleRotateCounterclockwise: function() {
+    this.props.handleRotate.call(this, -90);
+  },
+  handleZoomIn: function() {
+    this.props.handleZoom.call(this, 1);
+  },
+  handleZoomOut: function() {
+    this.props.handleZoom.call(this, -1);
+  },
+  render: function() {
+    return (
+      <div className='lightbox-modifiers-box'>
+        <button className='lightbox-btn' onClick={this.handleZoomIn}>
+          <i className='fa fa-search-plus'/>
+        </button>
+        <button className='lightbox-btn' onClick={this.handleZoomOut}>
+          <i className='fa fa-search-minus' />
+        </button>
+        <button className='lightbox-btn' onClick={this.handleRotateCounterclockwise}>
+          <i className='fa fa-rotate-left'/>
+        </button>
+        <button className='lightbox-btn' onClick={this.handleRotateClockwise}>
+          <i className='fa fa-rotate-right' />
+        </button>
+      </div>
+    )
+  }
+});
